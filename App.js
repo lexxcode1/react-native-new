21929be6b9b5e65268d3e5c7b2930fa0a16216b7
mally <diflamez@gmail.com>
2021-09-14 18:23:39 +0100

    Episode 2, using state

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+    const [name, setName] = useState('Shaun')
+    const [person, setPerson] = useState({name: 'Mario', age: 40})
+    const clickHandler = () => {
+        setName('chun-li')
+        setPerson({name:'luigi', age:45})
+    }
+
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.boldText}>Hello</Text>
-            </View>
-            <View style={styles.body}>
-                <Text>lorem</Text>
-                <Text>lorem</Text>
-                <Text>lorem</Text>
+            <Text>My name is {name}</Text>
+            <Text>His name is {person.name} and his age is {person.age}</Text>
+            <View style={styles.buttonContainer}>
+                <Button title={'Update Name'} onPress={clickHandler}/>
             </View>
         </View>
     );
@@ -24,15 +28,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    header: {
-        backgroundColor: 'pink',
-        padding: 20
-    },
-    boldText: {
-        fontWeight: 'bold'
-    },
-    body: {
-        backgroundColor: 'yellow',
-        padding: 20
+    buttonContainer: {
+        marginTop: 20
     }
+
 });
