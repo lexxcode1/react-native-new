5570906c07a61906ae631723e9e4f6850e6efee4
mally <diflamez@gmail.com>
2021-09-14 18:13:34 +0100

    Episode 1 completed

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.boldText}>Hello</Text>
+            </View>
+            <View style={styles.body}>
+                <Text>lorem</Text>
+                <Text>lorem</Text>
+                <Text>lorem</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        backgroundColor: 'pink',
+        padding: 20
+    },
+    boldText: {
+        fontWeight: 'bold'
+    },
+    body: {
+        backgroundColor: 'yellow',
+        padding: 20
+    }
 });
